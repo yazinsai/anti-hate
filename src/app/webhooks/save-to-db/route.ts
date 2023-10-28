6e312ec96f7acd100b2752b9639034f37fec93f4
Yazin <github-7itbcg5@yaz.in>
2023-10-28 20:12:24 +0300

    Drop use-server

--- src/app/webhooks/save-to-db/route.ts
+++ src/app/webhooks/save-to-db/route.ts
@@ -1,5 +1,3 @@
-'use server';
-
 /// Uses Data365 to retrieve the latest posts from LinkedIn and update the posts in the database
 /// This script is intended to be run on a schedule (e.g. daily) to keep the posts up to date
 
